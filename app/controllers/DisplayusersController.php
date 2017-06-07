6a294d0173ffccab3682c92f0971eaada288ff09
Vardhan Mogare <vardhan.mogare@gmail.com>
2017-06-07 17:38:11 +0530

    First commit

---
+++ app/controllers/DisplayusersController.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class DisplayusersController extends Controller
+{
+
+public function indexAction()
+{
+
+}
+}
+?>
---
+++ app/models/Users.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Users extends Model
+{
+    public $id;
+
+    public $firstname;
+
+    public $lastname;
+
+    public $email;
+
+    public $gender;
+
+    public $education;
+
+    public $skills;
+}
+
+?>
